3e452e96f0f29a1a13ee501dbb948174f9487e07
heosorio <hector.osorio@dreamcodesoft.com>
2025-12-03 16:37:49 -0500

    HACKA2025: Add bug memory leaks

--- src/main/java/com/hacka/demo/Bugs.java
+++ src/main/java/com/hacka/demo/Bugs.java
@@ -12,29 +12,6 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class Bugs {
-    private StatusLogger.Config config;
-    Connection conn;
-
-    private int value = 0;
-
-    public void increase() {
-        value++;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public String process() {
-        return config.getClass().getName();
-    }
-
-    public void executeQuery() throws SQLException {
-        String query = "SELECT * FROM usuario WHERE id = ?";
-        PreparedStatement ps = conn.prepareStatement(query);
-        ps.setString(1, "ABC");
-    }
-
     public void search(List<String> lista, String valor) throws InterruptedException {
         for (String s : lista) {
             if (s.equals(valor)) {
